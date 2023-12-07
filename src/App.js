87fc7eb46dd20e50ab9636f276badef0bafdaed2
marcelypcosta <amarcelypereiracosta@gmail.com>
2023-12-07 12:11:56 -0300

    Criando uma 'API' para buscar os livros

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Buscador from "./components/Buscador";
 const AppContainer = styled.div`
   font-family: Arial, Helvetica, sans-serif;
   width: 100%;
-  height: 100vh;
+  height: 200vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
 `;
 
--- src/components/Buscador/index.js
+++ src/components/Buscador/index.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import { livros } from "./dados";
+
 //Biblioteca
 import styled from "styled-components";
 
 // Components
 import Input from "../Input";
 
+
+// Estilização
 const SectionBuscar = styled.section`
   width: 80%;
   margin: 20px auto 0;
@@ -26,12 +31,36 @@ const Subtitulo = styled.h3`
   color: #fff;
 `;
 
+const LivrosContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: 20px;
+  margin-top: 30px;
+`;
+
 function Buscador() {
+  const [pesquisaLivros, setpesquisaLivros] = useState([]);
+
   return (
     <SectionBuscar>
       <Titulo>Já sabe por onde começar?</Titulo>
       <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-      <Input placeholder="Pesquise aqui seu próximo livro..." />
+      <Input
+        placeholder="Pesquise aqui seu próximo livro..."
+        onBlur={(event) => {
+          const txt = event.target.value;
+          const livroPesquisado = livros.filter((livro) =>
+            livro.nome.includes(txt)
+          );
+
+          setpesquisaLivros(livroPesquisado);
+        }}
+      />
+      <LivrosContainer>
+        {pesquisaLivros.map((livro) => (
+          <img src={livro.img} />
+        ))}
+      </LivrosContainer>
     </SectionBuscar>
   );
 }
